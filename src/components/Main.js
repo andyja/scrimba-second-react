82cbe39ba91d0c45bc66886d7780116fba80acfc
Andy Jackson <104866553+andyja@users.noreply.github.com>
2023-09-15 10:47:28 +0100

    list refactor

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,13 +4,13 @@ function MainCopy() {
     return (
         <main>
             <h1 className="main--title">Fun facts about React</h1>
-            <ol className="main--facts">
+            <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
                 <li>Is maintained by Meta</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ol>
+            </ul>
         </main>
     )
 }
